c7661d69501a7d8df3f65c07ef305d0fbe6c8e65
NatsukiAsuka <NatsukiAsuka1973@gmail.com>
2018-06-08 14:21:07 +0800

    solve max sub array

---
+++ src/solutions/maxSubArray.go
@@ -0,0 +1,17 @@
+package solutions
+
+import "math"
+
+func MaxSubArray(nums []int) int {
+	max := math.MinInt32
+	num := 0
+	for _, v := range nums {
+		if num < 0 {
+			num = v
+		} else {
+			num += v
+		}
+		max = int(math.Max(float64(max), float64(num)))
+	}
+	return max
+}
