71a2d1e5c5555d3ea845885730fecfa9fd851a9e
NatsukiAsuka <NatsukiAsuka1973@gmail.com>
2018-06-12 00:43:21 +0800

    solve num squares

---
+++ src/solutions/numSquares.go
@@ -0,0 +1,26 @@
+package solutions
+
+import (
+	"math"
+)
+
+func numSquares(n int) int {
+	var arr []int
+	for i := 1; i*i <= n; i++ {
+		arr = append(arr, i*i)
+	}
+
+	dp := make([]int, n+1)
+	for i := 1; i < len(dp); i++ {
+		dp[i] = math.MaxInt32
+	}
+
+	for _, v := range arr {
+		for i := v; i < len(dp); i++ {
+			if dp[i] > dp[i-v]+1 {
+				dp[i] = dp[i-v] + 1
+			}
+		}
+	}
+	return dp[n]
+}
