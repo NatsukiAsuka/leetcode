80db6af52d409c2a48894678f1433e49a72145cc
NatsukiAsuka <NatsukiAsuka1973@gmail.com>
2018-06-13 16:33:17 +0800

    solve reverse

---
+++ src/solutions/reverse.go
@@ -0,0 +1,17 @@
+package solutions
+
+import "math"
+
+func Reverse(x int) int {
+	ans := 0
+	n := 0
+	for x != 0 {
+		if ans > math.MaxInt32/10 || ans < math.MinInt32/10 {
+			return 0
+		}
+		n = x % 10
+		ans = ans*10 + n
+		x = x / 10
+	}
+	return ans
+}
