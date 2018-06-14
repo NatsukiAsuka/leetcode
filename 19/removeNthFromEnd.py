3b24cd1b4f3f5075fccf64507e00d8b82f28bc08
NatsukiAsuka <NatsukiAsuka1973@gmail.com>
2018-06-14 11:19:39 +0800

    update

---
+++ 19/removeNthFromEnd.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        node = ListNode(0)
+        node.next = head
+        fast = slow = node
+        for _ in range(n + 1):
+            fast = fast.next
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return node.next
+
