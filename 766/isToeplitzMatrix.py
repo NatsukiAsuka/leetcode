ffe6a58edcb7dc57ae8a894480e26808424c9379
NatsukiAsuka <NatsukiAsuka1973@gmail.com>
2018-06-17 11:48:14 +0800

    update

--- 766/isToeplitzMatrix.py
+++ 766/isToeplitzMatrix.py
@@ -1,12 +1,13 @@
-def isToeplitzMatrix(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: bool
-        """
-        n = len(matrix)
-        m = len(matrix[0])
-
-        for i in range(0, n - 1):
-            for j in range(0, m - 1):
-                if matrix[i][j] != matrix[i + 1][j + 1]: return False
-        return True
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+            """
+            :type matrix: List[List[int]]
+            :rtype: bool
+            """
+            n = len(matrix)
+            m = len(matrix[0])
+    
+            for i in range(0, n - 1):
+                for j in range(0, m - 1):
+                    if matrix[i][j] != matrix[i + 1][j + 1]: return False
+            return True
